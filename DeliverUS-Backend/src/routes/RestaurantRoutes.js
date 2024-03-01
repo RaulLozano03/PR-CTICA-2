8f55afe10f561ba792c8e52fcbd03b20f212cc04
RaulLozano03 <raralolosasa@gmail.com>
2024-03-01 12:37:53 +0100

    avance

--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -2,6 +2,23 @@ import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
   // TODO: Include routes for restaurant described in the lab session README
+  app.route('/restaurants')
+    .get(
+      RestaurantController.index
+    )
+    .post(
+      RestaurantController.create
+    )
 
+  app.route('/restaurants/:restaurantId')
+    .put(
+      RestaurantController.update
+    )
+    .get(
+      RestaurantController.show
+    )
+    .delete(
+      RestaurantController.destroy
+    )
 }
 export default loadFileRoutes
